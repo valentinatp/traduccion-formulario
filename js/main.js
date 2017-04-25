c25b8e4ba656fbe8f074782ff0dbf325e933a91f
valentinatp <valentina.toledoporma@gmail.com>
2017-04-25 12:29:52 -0300

    Comienzo a crear la funcion 'translate' en el archivo js, para poder traducir el html

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function translate(){
+	var title = document.getElementById("form-signin-heading");
+	var email = document.getElementById("inputEmail");
+	var password = document.getElementById("inputPassword");
+	var remember = document.getElementsByTagName("span")[0];
+	var button = document.getElementsByClassName("btn")[0];
+
+	title.innerHTML = "Por favor inicia sesión";
+	email.placeholder = "Correo Electrónico";
+	password.placeholder = "Contraseña";
+	remember.innerHTML = "Recordar datos";
+	button.innerHTML = "Iniciar Sesión";
+}
+
+translate();
